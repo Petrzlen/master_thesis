ab952d47b3e4afa12cd743021fa01da5fe8c81ef
Peter Csiba <petherz@gmail.com>
2014-04-25 00:08:18 +0200

    Adding TLR auto4 success plots

--- bal/data/bozi_trosku.bash
+++ bal/data/bozi_trosku.bash
@@ -1,7 +1,39 @@
 #!/bin/bash
 
-gnuplot -e "inpath='stats/auto4_bal_orig_back/log_lls_0.dat';outpath='../../text/img/tlr-auto4-success.pdf';val_from=0;val_d=5;val_to=100;" contour.p
+#=================== TLR ====================
+  #=================== auto4 ====================
+  echo "tlr-auto4-success.pdf"
+gnuplot -e "inpath='stats/auto4_bal_orig/log_lls_0.dat';outpath='../../text/img/tlr-auto4-success.pdf';val_from=0;val_d=5;val_to=100;rxf=-4;rxt=9;ryf=-9;ryt=1;" contour.p
 
-awk '{if(NF==2) print $1,$2,50000; else print $0}' stats/auto4_bal_orig_back/log_lle_0.dat | awk '{print $1,$2,($3>10000)?10000:$3}' > buf.dat
-gnuplot -e "inpath='buf.dat';outpath='../../text/img/tlr-auto4-epoch.pdf';val_from=0;val_d=-1000;val_to=10000;" contour.p
+  echo "tlr-auto4-epoch.pdf"
+awk '{if(NF==2) print $1,$2,50000; else print $0}' stats/auto4_bal_orig/log_lle_0.dat | awk '{print $1,$2,($3>10000)?10000:$3}' > buf.dat
+gnuplot -e "inpath='buf.dat';outpath='../../text/img/tlr-auto4-epoch.pdf';val_from=0;val_d=1000;val_to=10000;rxf=-4;rxt=9;ryf=-9;ryt=1;" contour.p
+
+#=================== BAL RECIRC ====================
+  #=================== auto4 ====================
+#awk '{if(NF<=1||(-3.5<$1 && $1<2)) print $0}' stats/auto4_bal_recirc/log_lls_0.dat > buf.dat
+
+  echo "bal-recirc-auto4-success.pdf"
+gnuplot -e "inpath='stats/auto4_bal_recirc/log_lls_0.dat';outpath='../../text/img/bal-recirc-auto4-success.pdf';val_from=0;val_d=5;val_to=100;rxf=-4;rxt=2;ryf=-9;ryt=2;" contour.p
+
+  echo "bal-recirc-auto4-epoch.pdf"
+awk '{if(NF==2) print $1,$2,50000; else print $0}' stats/auto4_bal_recirc/log_lle_0.dat | awk '{print $1,$2,($3>10000)?10000:$3}' > buf.dat
+gnuplot -e "inpath='buf.dat';outpath='../../text/img/bal-recirc-auto4-epoch.pdf';val_from=0;val_d=1000;val_to=10000;rxf=-4;rxt=2;ryf=-9;ryt=2;" contour.p
+
+#=================== BAL RECIRC ====================
+  #=================== auto4 ====================
+#awk '{if(NF<=1||(-3.5<$1 && $1<2)) print $0}' stats/auto4_bal_recirc/log_lls_0.dat > buf.dat
+
+  echo "generec-auto4-success.pdf"
+gnuplot -e "inpath='stats/auto4_generec/log_lls_0.dat';outpath='../../text/img/generec-auto4-success.pdf';val_from=0;val_d=5;val_to=100;rxf=-4;rxt=2;ryf=-7;ryt=2;" contour.p
+
+  echo "generec-auto4-epoch.pdf"
+awk '{if(NF==2) print $1,$2,50000; else print $0}' stats/auto4_generec/log_lle_0.dat | awk '{print $1,$2,($3>20000)?20000:$3}' > buf.dat
+gnuplot -e "inpath='buf.dat';outpath='../../text/img/generec-auto4-epoch.pdf';val_from=0;val_d=1000;val_to=10000;rxf=-4;rxt=2;ryf=-7;ryt=2;" contour.p
+
+ls | grep -o 'k3_139[0-9]\+_[0-9]\+' | sort | uniq | while read filename
+do
+  echo "motam $filename"
+  bash zmotaj_stats.bash $filename $filename 
+done 
 
--- bal/data/contour.p
+++ bal/data/contour.p
@@ -5,8 +5,9 @@
 #http://www.dommelen.net/l2h/contour.txt
 #http://gnuplot-tricks.blogspot.sk/2009/07/maps-contour-plots-with-labels.html
 
-#params inpath, outpath
-#params val_from, val_d, val_to
+#params inpath, outpath (files) 
+#params val_from, val_d, val_to (contour) 
+#params rxf,rxt, ryf,ryt (ranges) 
 
 reset
 #set xtic auto  
@@ -19,6 +20,8 @@ reset
 #set dgrid3d 
 
 #TODO logscale 
+set xrange [rxf:rxt]
+set yrange [ryf:ryt]
 
 set table 'cont-iso.dat'
 splot inpath
@@ -33,19 +36,20 @@ unset table
 
 reset
 unset key
-set xrange [-4:9]
-set yrange [-9:1.5]
+set xrange [rxf:rxt]
+set yrange [ryf:ryt]
 
 set xtics ("0.1^9" -9, "0.1^8" -8, "0.1^7" -7, "0.1^6" -6, "0.1^5" -5, "0.1^4" -4, "0.001" -3, "0.01" -2, "0.1" -1, "1" 0, "10" 1, "100" 2, "1000" 3, "10^4" 4, "10^5" 5, "10^6" 6, "10^7" 7, "10^8" 8, "10^9" 9) rotate by 270
 set ytics ("0.1^9" -9, "0.1^8" -8, "0.1^7" -7, "0.1^6" -6, "0.1^5" -5, "0.1^4" -4, "0.001" -3, "0.01" -2, "0.1" -1, "1" 0, "10" 1, "100" 2, "1000" 3, "10^4" 4, "10^5" 5, "10^6" 6, "10^7" 7, "10^8" 8, "10^9" 9) 
 #set key font "Times-Roman, 15" 
-set terminal pdf #transparent enhanced font "arial,10" #size 500, 350 
+set terminal pdf font "arial,5" #size 500, 350 
 set output outpath
 
 #http://stackoverflow.com/questions/19294342/heatmap-with-gnuplot-on-a-non-uniform-gridless
 
+#set pm3d interpolate 4,4
 set palette rgbformulae 33,13,10
-l '<./contour.sh cont-line.dat 10 25 7'
+l '<./contour.sh cont-line.dat 10 20 7'
 p inpath u 1:2:3 with image, 'cont-line.dat' w l lt -1 lw 1.5
 
 #set terminal pngcairo enhanced font "arial,10" size 800, 800 
